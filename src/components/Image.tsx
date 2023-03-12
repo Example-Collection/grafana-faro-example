4636c684a44d8a79c3e0c9397745a230ebe45fb0
sang-w0o <robbyra@gmail.com>
2023-03-12 19:07:50 +0900

    feat: Add common styled componentes for images

---
+++ src/components/Image.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #161b22;
+  font-family: "Roboto", sans-serif;
+  border-radius: 8px;
+  margin-top: 1rem;
+`;
+
+export const ImageDescription = styled.div`
+  font-size: 1rem;
+  color: #c9d1d9;
+  font-family: "Roboto", sans-serif;
+  margin-top: 1rem;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+`;
